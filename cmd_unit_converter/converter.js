c66e2891d23e52fb700630a85320a285ccfc94a0
Mavis <llingchen.27@gmail.com>
2025-05-20 09:18:27 +0800

    新增換算methods，讓主要執行涵式程式碼更簡潔

--- cmd_unit_converter/converter.js
+++ cmd_unit_converter/converter.js
@@ -1,47 +1,83 @@
+/**
+ * 單位換算
+ */
 class UnitConverter {
 
-    constructor(){
-        this.value = 0;
-        this.fromUnit = "";
-        this.toUnit = "";
+    /**
+     * 
+     * @param {number} value 數值
+     * @param {string} from 原始單位
+     * @param {string} to 欲換算單位
+     */
+    constructor(value,from,to){
+        this.value = value;
+        this.fromUnit = from;
+        this.toUnit = to;
     }
-    // 公尺 => 英尺
-    metersToFeet(value) {
-        return value * 3.28084;
+    /**
+     * 公尺 => 英尺
+     */
+    metersToFeet() {
+        return this.value * 3.28084;
     }
-    // 英呎 => 公尺
-    feetToMeters(value) {
-       return value / 3.28084;
+    /**
+     * 英呎 => 公尺
+     */
+    feetToMeters() {
+       return this.value / 3.28084;
     }
-    // 公尺 => 公分
-    meterToCenimeter(value) {
-        return value * 100;
+    /**
+     * 公尺 => 公分
+     */ 
+    meterToCenimeter() {
+        return this.value * 100;
     }
-    // 公分 => 公尺
-    cenimeterToMetet(value) {
-        return value / 100;
+    /**
+     * 公分 => 公尺
+     */
+    cenimeterToMetet() {
+        return this.value / 100;
     }
 
-    // 公斤 => 磅
-    kilogramsToPounds(value) {
-        return value * 2.20462;
+    /**
+     * 公斤 => 磅
+     */
+    kilogramsToPounds() {
+        return this.value * 2.20462;
     }
-    // 磅 => 公斤
-    poundsToKilograms(value) {
-        return value / 2.20462;
+    /**
+     * 磅 => 公斤
+     */
+    poundsToKilograms(){
+        return this.value / 2.20462;
     }
 
+    /**
+     * 依據fromUnit、toUnit執行各methods換算
+     * @returns 
+     */
+    doConverter(){
 
-    // getResult(value,from,to){
-    //     this.value = value;
-    //     this.fromUnit = from;
-    //     this.toUnit = to;
-    //     if(this.fromUnit === "cenimeter" && this.toUnit === "meters"){
-    //         return this.cenimeterToMetet(value);
-    //     }else if(this.fromUnit === "meters" && this.toUnit === "cenimeter"){
-    //         return this.meterToCenimeter(value);
-    //     }
-    // }
+        if ((this.fromUnit === "meters" && this.toUnit === "feet") || (this.fromUnit === "m" && this.toUnit === "ft")){
+            return this.metersToFeet()
+
+        } else if ((this.fromUnit === "feet" && this.toUnit === "meters") || (this.fromUnit === "ft" && this.toUnit === "m")) {
+            return this.feetToMeters()
+            
+        } else if((this.fromUnit === "meters" && this.toUnit === "centimeters") || (this.fromUnit === "m" && this.toUnit === "cm")) {
+            return this.meterToCenimeter();
+            
+        }else if((this.fromUnit === "centimeters" && this.toUnit === "meters") || (this.fromUnit === "cm" && this.toUnit === "m")) {
+            return this.cenimeterToMetet();
+            
+        }else if((this.fromUnit === "kg" && this.toUnit === "lb") || (this.fromUnit === "kilograms" && this.toUnit === "pounds")) {
+            return this.kilogramsToPounds();
+
+        }else if((this.fromUnit === "lb" && this.toUnit === "kg") || (this.fromUnit === "pounds" && this.toUnit === "kilograms")) {
+            return this.poundsToKilograms();
+        }
+
+    }
 }
 
 export default UnitConverter;
--- cmd_unit_converter/index.js
+++ cmd_unit_converter/index.js
@@ -1,32 +1,33 @@
 import UnitConverter from "./converter.js";
 
 /**
- * 長度換算
- * @param {number} value
- * @param {string} fromUnit
- * @param {string} toUnit
- */
-// function convertLength (value,fromUnit,toUnit) {
-//     if (fromUnit === 'meters' && toUnit === 'centimeters') {
-//         return value * 100;
-//     } else if (fromUnit === 'centimeters' && toUnit === 'meters') {
-//         return value / 100;
-//     } else {
-//         return null; // 表示不支持的轉換
-//     }
-// }
-
-/**
- * 重量換算
- * 
- * @param {number} value 
+ * 單位換算
+ * @param {number} unit 
  * @param {string} fromUnit 
  * @param {string} toUnit 
+ * @returns 
  */
-// function convertWeight (value,fromUnit,toUnit) {
+function converter(unit,fromUnit,toUnit){
+   if ((fromUnit === "meters" && toUnit === "feet") || (fromUnit === "m" && toUnit === "ft")){
+        return unit * 3.28084;
+   } else if ((fromUnit === "feet" && toUnit === "meters") || (fromUnit === "ft" && toUnit === "m")) {
+       return unit  / 3.28084;
 
-// }
+    } else if((fromUnit === "meters" && toUnit === "centimeters") || (fromUnit === "m" && toUnit === "cm")) {
+        return unit * 100;
 
+    }else if((fromUnit === "centimeters" && toUnit === "meters") || (fromUnit === "cm" && toUnit === "m")) {
+        return unit / 100;
+
+    }else if((fromUnit === "kg" && toUnit === "lb") || (fromUnit === "kilograms" && toUnit === "pounds")){
+        return unit * 2.20462
+
+    }else if((fromUnit === "lb" && toUnit === "kg") || (fromUnit === "pounds" && toUnit === "kilograms")){
+       return unit / 2.20462;
+    }else{
+        console.log("請確認單位英文是否正確。")
+    }
+}
 /**
  * 輸入參數型態驗證
  * 
@@ -50,22 +51,22 @@ function checkType(value,fromUnit,toUnit){
  */
 function abbreviationParam (unit){
     switch (unit.toLowerCase) {
-        case 'm':
-            return 'meters';
-        case 'meters':
-            return 'meters';
-        case 'cm':
-            return 'centimeters';
-        case 'centimeters':
-            return 'centimeters';
-        case 'kg':
-            return 'kilograms';
-        case 'kilograms':
-            return 'kilograms';
-        case 'g':
-            return 'grams';
-        case 'grams':
-            return 'grams';
+        case "m":
+        case "meters":
+            return "meters";
+        case "cm":
+        case "centimeters":
+            return "centimeters";
+        case "kg":
+        case "kilograms":
+            return "kilograms";
+        case "g":
+        case "grams":
+            return "grams";
+        case "lbs":
+        case "lb":
+        case "pounds":
+            return "pounds";
         default:
             return unit;
     }
@@ -76,7 +77,6 @@ function abbreviationParam (unit){
  * @returns 
  */
 function showDescriptions(paramLen){
-    // console.log(paramLen.length)
 //   process.exit(1);
   const descriptions = [
     "請使用以下指令：",
@@ -98,41 +98,28 @@ function showDescriptions(paramLen){
  * eg. 5 cm to m
  * 
  * value: 數值
- * fromUnit: 原始單位；"
+ * fromUnit: 原始單位；
  * toUnit: 目標單位
  */
 async function main() {
     
     const args = process.argv.slice(2);
-    const value = parseFloat(process.argv[2]);
+    showDescriptions(args);
 
-    abbreviationParam();
-    const fromUnit = process.argv[3].toLowerCase();
-    const toUnit = process.argv[5].toLowerCase();
+    const value = parseFloat(process.argv[2]);
+    const fromUnit = abbreviationParam(process.argv[3]);
+    const toUnit = abbreviationParam(process.argv[5]);
     let result = 0;
     
-    showDescriptions(args);
     if(checkType(value,fromUnit,toUnit)){
         // 各種換算
-        let converter = new UnitConverter();
-        if (fromUnit === 'meters' && toUnit === 'feet') {
-            result = converter.metersToFeet(value);
-        } else if (fromUnit === 'feet' && toUnit === 'meters') {
-            result = converter.feetToMeters(value);
-        } else if(fromUnit === 'meters' && toUnit === 'centimeters'){
-            result = converter.meterToCenimeter(value);
-        }else if(fromUnit === 'centimeters' && toUnit === 'meters'){
-            result = converter.cenimeterToMetet(value);
-        }
+        // result = converter(value,fromUnit,toUnit);
 
-        // if (fromUnit === 'meters' || fromUnit === 'centimeters') {
-        //     result = convertLength(value, fromUnit, toUnit);
-        // } else if (fromUnit === 'kg' || fromUnit === 'grams') {
-        //     result = convertWeight(value, fromUnit, toUnit);
-        // }
+        // oop
+        let converter = new UnitConverter(value,fromUnit,toUnit);
+        result = converter.doConverter();
+
         console.log(`${value} ${fromUnit} 換算成 ${toUnit} 是 ${result} ${toUnit}`);
     }
-    
-    
 }
 main();
